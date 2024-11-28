322033696e888283c4029ef40ac9bd13dbda5a91
Roberto Nogueira <enogrob@gmail.com>
2024-11-27 21:58:54 -0300

    controllers-setup

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,31 @@
+class OrdersController < ApplicationController
+  def upload
+    file = params[:file]
+    render json: NormalizeFileService.process(file.path)
+  end
+
+  def index
+    orders = Order.includes(:products, :user).all
+
+    # Adicione filtros de id e data
+    orders = orders.where(order_id: params[:id]) if params[:id]
+    if params[:start_date] && params[:end_date]
+      orders = orders.where(date: params[:start_date]..params[:end_date])
+    end
+
+    render json: orders.map { |order| serialize_order(order) }
+  end
+
+  private
+
+  def serialize_order(order)
+    {
+      order_id: order.order_id,
+      total: "%.2f" % order.total,
+      date: order.date,
+      products: order.products.map do |product|
+      { product_id: product.product_id, value: "%.2f" % product.value }
+      end
+    }
+  end
+end
